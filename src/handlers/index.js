1ccd799f726b5cbb3110b8289902c17994f31975
João Vitor Retamero <joaovretamero@gmail.com>
2020-01-20 22:47:51 -0300

    Route aggregation

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -7,11 +7,19 @@ const bookHandlers = require("./books");
 const Book = require("../models/book");
 
 router.get("/", homeHandlers.home);
-router.get("/books", bookHandlers.listBooks);
-router.post("/books", Book.validations(), bookHandlers.createBook);
-router.put("/books", bookHandlers.updateBook);
-router.get("/books/new", bookHandlers.newBook);
-router.get("/books/:id", bookHandlers.detailBook);
-router.delete("/books/:id", bookHandlers.deleteBook);
+
+const booksRouter = express.Router();
+booksRouter.route("/")
+    .get(bookHandlers.listBooks)
+    .post(Book.validations(), bookHandlers.createBook)
+    .put(bookHandlers.updateBook);
+
+booksRouter.get("/new", bookHandlers.newBook);
+
+booksRouter.route("/:id")
+    .get(bookHandlers.detailBook)
+    .delete(bookHandlers.deleteBook);
+
+router.use("/books", booksRouter);
 
 module.exports = router;
